533bc871bfa60bf30e96f40ad2f1042d2085d7bc
ryanhoo <ryan.hoo.j@gmail.com>
2014-09-10 17:36:09 +0800

    feat(app details): display an app

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -2,18 +2,20 @@ angular.module('app.controllers', [])
 
 .controller('AppListCtrl', ['$scope', 'App',
   function($scope, App) {
-    App.query().$promise.then(function(data){
+    App.query().$promise.then(function(data) {
       $scope.appList = data
-    }).catch(function(resp){
+    }).catch(function(resp) {
       console.log(resp)
     })
 
     $scope.deleteApp = function(id) {
       var result = window.confirm('确定要删除吗？')
       if (result) {
-        App.delete({app_id: id}).$promise.then(function(data){
+        App.delete({
+          app_id: id
+        }).$promise.then(function(data) {
           location.reload()
-        }).catch(function(resp){
+        }).catch(function(resp) {
           console.log(resp)
           alert('删除失败！')
         })
@@ -24,18 +26,28 @@ angular.module('app.controllers', [])
 
 .controller('CreateAppCtrl', ['$scope', 'App',
   function($scope, App) {
-    App.save({'name': '周末去哪儿 - v2.2.1', 'platforms': ['android']}).$promise.then(function(data){
+    App.save({
+      'name': '周末去哪儿 - v2.2.1',
+      'platforms': ['android']
+    }).$promise.then(function(data) {
       $scope.txt = data
       console.log(data)
-    }).catch(function(resp){
+    }).catch(function(resp) {
       $scope.txt = resp
       console.log(resp)
     })
   }
 ])
 
-.controller('AppDetailsCtrl', ['$scope',
-  function($scope) {
-    $scope.txt = 'hello this is an app details'
+.controller('AppDetailsCtrl', ['$scope', '$routeParams', 'App',
+  function($scope, $routeParams, App) {
+    $scope.app = new App({
+      id: $routeParams.app_id
+    })
+    $scope.app.$get({app_id:$routeParams.app_id}).then(function(data) {
+      console.log(data)
+    }).catch(function(resp) {
+      console.log(resp)
+    })
   }
 ])
